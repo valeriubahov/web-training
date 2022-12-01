f3fe4548a250c2b126a639f7922e0739a8d2fe01
Valeriu <valeriu.bahov@redspace.com>
2022-12-01 12:09:51 -0400

    Create a list of expenses using components and css

---
+++ expenses/src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+// NOTES: 
+// A react component is just a javascript function
+// every component must be exported in order to be able to use it in other components
+
+import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+
+function ExpenseItem(props) {
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+    </Card>
+  )
+}
+
+export default ExpenseItem;

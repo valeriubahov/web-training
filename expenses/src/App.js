b28b1fbc068c69037d2784acd47750d4b51e4a1d
Valeriu <valeriu.bahov@redspace.com>
2022-12-01 12:19:38 -0400

    Organizing files

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -1,4 +1,4 @@
-import ExpensesList from "./components/ExpensesList";
+import ExpensesList from "./components/Expenses/ExpensesList";
 
 function App() {
   const expenses = [
--- expenses/src/components/Expenses/ExpenseItem.js
+++ expenses/src/components/Expenses/ExpenseItem.js
@@ -4,7 +4,7 @@
 
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 function ExpenseItem(props) {
   return (

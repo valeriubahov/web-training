2020097bfbd7d5e8ba4a783c8d9ba367bde8fff0
Valeriu <valeriu.bahov@redspace.com>
2022-12-12 14:28:31 -0400

    Added cart login, close and open modal

--- food-order-app/src/components/Layout/Header.jsx
+++ food-order-app/src/components/Layout/Header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>FoodTraining</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />

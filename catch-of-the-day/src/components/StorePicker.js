86a154faeb454fb582ae42de5ef199344042db8b
Valeriu <valeriu.bahov@redspace.com>
2022-12-14 15:53:05 -0400

    set router to move between pages

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please enter a store</h2>
+        <input
+          ref={this.myInput}
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
